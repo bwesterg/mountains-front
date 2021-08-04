363739e3c5956eb8fc6d6327e9c7f798d98c8066
bwesterg <benton.westergaard@gmail.com>
2021-08-04 16:59:19 -0500

    fighting airport wifi.

--- src/components/MountainItem.js
+++ src/components/MountainItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function MountainItem(props) {
     return(
         <li>
-            Mountain Item
+            Mountain Item test 1234
         </li>
     )
 }
